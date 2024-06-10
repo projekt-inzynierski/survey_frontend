ea154b85b6b92ebc37316a081014b249d8ef3249
Mateusz Dokowicz <mat.dok123@gmail.com>
2024-06-10 20:51:29 +0200

    rename home service to short survey

--- lib/domain/external_services/home_service.dart
+++
@@ -1,6 +0,0 @@
-import 'package:survey_frontend/domain/external_services/api_response.dart';
-import 'package:survey_frontend/domain/models/short_survey_dto.dart';
-
-abstract class HomeService {
-  Future<APIResponse<List<ShortSurveyDto>>> getShortSurvey();
-}
