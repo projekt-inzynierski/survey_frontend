c6aac55d4b88ab9229e0c9281a3cc46989e24f60
Mateusz Dokowicz <mat.dok123@gmail.com>
2024-06-10 22:20:13 +0200

    use real values

--- lib/data/datasources/short_survey_service_impl.dart
+++ lib/data/datasources/short_survey_service_impl.dart
@@ -10,23 +10,24 @@ class ShortSurveyServiceImpl extends APIServiceBase
     implements ShortSurveyService {
   ShortSurveyServiceImpl(super.dio);
 
-  @override
-  Future<APIResponse<List<ShortSurveyDto>>> getShortSurvey() async {
-    final jsonString =
-        await rootBundle.loadString("assets/mocked/pending_survey.json");
-    final List<dynamic> json = jsonDecode(jsonString);
-
-    final List<ShortSurveyDto> items =
-        json.map<ShortSurveyDto>((e) => ShortSurveyDto.fromJson(e)).toList();
-
-    return APIResponse(body: items);
-  }
-
+  // Mocked Data
   // @override
-  // Future<APIResponse<List<ShortSurveyDto>>> getShortSurvey() =>
-  //     get<List<ShortSurveyDto>>(
-  //         '/api/surveys/shortsummaries',
-  //         (dynamic items) => items
-  //             .map<ShortSurveyDto>((e) => ShortSurveyDto.fromJson(e))
-  //             .toList());
+  // Future<APIResponse<List<ShortSurveyDto>>> getShortSurvey() async {
+  //   final jsonString =
+  //       await rootBundle.loadString("assets/mocked/pending_survey.json");
+  //   final List<dynamic> json = jsonDecode(jsonString);
+
+  //   final List<ShortSurveyDto> items =
+  //       json.map<ShortSurveyDto>((e) => ShortSurveyDto.fromJson(e)).toList();
+
+  //   return APIResponse(body: items);
+  // }
+
+  @override
+  Future<APIResponse<List<ShortSurveyDto>>> getShortSurvey() =>
+      get<List<ShortSurveyDto>>(
+          '/api/surveys/short',
+          (dynamic items) => items
+              .map<ShortSurveyDto>((e) => ShortSurveyDto.fromJson(e))
+              .toList());
 }
