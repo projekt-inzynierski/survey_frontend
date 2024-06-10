b8c4c3735c0c09c433fbfb60f8750f6c0400b2c0
Mateusz Dokowicz <mat.dok123@gmail.com>
2024-06-10 22:20:13 +0200

    use real values

---
+++ lib/data/datasources/short_survey_service_impl.dart
@@ -0,0 +1,34 @@
+
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+import 'package:survey_frontend/data/datasources/api_service_base.dart';
+import 'package:survey_frontend/domain/external_services/api_response.dart';
+import 'package:survey_frontend/domain/external_services/short_survey_service.dart';
+import 'package:survey_frontend/domain/models/short_survey_dto.dart';
+
+class ShortSurveyServiceImpl extends APIServiceBase
+    implements ShortSurveyService {
+  ShortSurveyServiceImpl(super.dio);
+
+  // Mocked Data
+  // @override
+  // Future<APIResponse<List<ShortSurveyDto>>> getShortSurvey() async {
+  //   final jsonString =
+  //       await rootBundle.loadString("assets/mocked/pending_survey.json");
+  //   final List<dynamic> json = jsonDecode(jsonString);
+
+  //   final List<ShortSurveyDto> items =
+  //       json.map<ShortSurveyDto>((e) => ShortSurveyDto.fromJson(e)).toList();
+
+  //   return APIResponse(body: items);
+  // }
+
+  @override
+  Future<APIResponse<List<ShortSurveyDto>>> getShortSurvey() =>
+      get<List<ShortSurveyDto>>(
+          '/api/surveys/short',
+          (dynamic items) => items
+              .map<ShortSurveyDto>((e) => ShortSurveyDto.fromJson(e))
+              .toList());
+}
