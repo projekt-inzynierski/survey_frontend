048706b0c56e6317414363eaa1eb4ba1bea8d82c
Mathew <106106919+mdokusV@users.noreply.github.com>
2024-06-11 21:56:36 +0200

    #60: Show real value of possible surveys in Home page (#10)
    * fix next question check + separate QuestionType + delete unnecessary code
    
    * use mocked short survey
    
    * rename home service to short survey
    
    * use real values
    
    * swap startScreenPath

--- lib/presentation/bindings/home_bindings.dart
+++ lib/presentation/bindings/home_bindings.dart
@@ -1,11 +1,12 @@
 import 'package:get/get.dart';
+import 'package:survey_frontend/data/datasources/short_survey_service_impl.dart';
+import 'package:survey_frontend/domain/external_services/short_survey_service.dart';
 import 'package:survey_frontend/presentation/controllers/home_controller.dart';
-import 'package:survey_frontend/presentation/controllers/survey_question_controller.dart';
 
 class HomeBindings extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<HomeController>(() => HomeController());
-    Get.lazyPut(() => SurveyController(Get.find()));
+    Get.lazyPut<ShortSurveyService>(() => ShortSurveyServiceImpl(Get.find()));
+    Get.lazyPut<HomeController>(() => HomeController(Get.find()));
   }
 }
--- lib/presentation/controllers/home_controller.dart
+++ lib/presentation/controllers/home_controller.dart
@@ -1,15 +1,30 @@
 import 'package:get/get.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:survey_frontend/domain/external_services/api_response.dart';
+import 'package:survey_frontend/domain/external_services/short_survey_service.dart';
+import 'package:survey_frontend/domain/models/short_survey_dto.dart';
 import 'package:survey_frontend/presentation/controllers/controller_base.dart';
 
 class HomeController extends ControllerBase {
-  RxList<SurveyShortInfo> pendingSurveys = [
-    SurveyShortInfo(
-        name: 'Some pending survey', id: 'assets/mocked/survey.json'),
-    SurveyShortInfo(
-        name: 'Another pending survey', id: 'assets/mocked/survey.json'),
-    SurveyShortInfo(
-        name: 'Yet another pending survey', id: 'assets/mocked/survey.json'),
-  ].obs;
+  final ShortSurveyService _homeService;
+  RxList<ShortSurveyDto> pendingSurveys = <ShortSurveyDto>[].obs;
+
+  HomeController(this._homeService) {
+    _loadShortSurveys();
+  }
+
+  Future<void> _loadShortSurveys() async {
+    pendingSurveys.clear();
+    APIResponse<List<ShortSurveyDto>> response =
+        await _homeService.getShortSurvey();
+    if (response.error != null || response.body == null) {
+      // TODO: swap for "response.statusCode != 200"
+      await handleSomethingWentWrong(null);
+      return;
+    }
+    pendingSurveys.addAll(response.body!);
+  }
+  
   final RxInt hours = 5.obs;
   final RxInt minutes = 13.obs;
 }
