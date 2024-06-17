ee252551070d0a20475ded44c0f5a1cccea784b7
Mateusz Dokowicz <mat.dok123@gmail.com>
2024-06-17 05:13:32 +0200

    quickFiX multiple survey completion

--- lib/presentation/bindings/survey_end_bindings.dart
+++ lib/presentation/bindings/survey_end_bindings.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:survey_frontend/data/datasources/local/survey_participation_service_impl.dart';
 import 'package:survey_frontend/data/datasources/survey_response_service_impl.dart';
 import 'package:survey_frontend/domain/external_services/survey_response_service.dart';
+import 'package:survey_frontend/domain/local_services/survey_participation_service.dart';
 import 'package:survey_frontend/domain/usecases/token_provider.dart';
 import 'package:survey_frontend/presentation/controllers/survey_end_controller.dart';
 
@@ -9,6 +11,9 @@ class SurveyEndBindings extends Bindings{
   void dependencies() {
     Get.lazyPut<SurveyResponseService>(() => SurveyResponseServiceImpl(Get.find(), tokenProvider: Get.find<TokenProvider>()));
     Get.lazyPut(() => SurveyEndController(Get.find(), Get.find()));
+    Get.lazyPut<SurveyParticipationService>(
+        () => SurveyParticipationServiceImpl(Get.find()));
+
   }
 
 }
