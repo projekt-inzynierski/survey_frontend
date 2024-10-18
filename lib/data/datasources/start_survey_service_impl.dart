6785ee13283974bb446b081978dff492b7e90bf1
Pawel Felcyn <pawel.felcyn@onet.pl>
2024-10-18 20:08:41 +0200

    create models and service

---
+++ lib/data/datasources/start_survey_service_impl.dart
@@ -0,0 +1,13 @@
+import 'package:survey_frontend/data/datasources/api_service_base.dart';
+import 'package:survey_frontend/domain/external_services/api_response.dart';
+import 'package:survey_frontend/domain/external_services/start_survey_service.dart';
+import 'package:survey_frontend/domain/models/start_survey_question.dart';
+
+class StartSurveyServiceImpl extends APIServiceBase implements StartSurveyService {
+  StartSurveyServiceImpl(super.dio, {required super.tokenProvider});
+
+  @override
+  Future<APIResponse<List<StartSurveyQuestion>>> getStartSurvey() 
+   => get<List<StartSurveyQuestion>>('/api/qualityofsleep',  (dynamic items) => items.map<StartSurveyQuestion>((e) => StartSurveyQuestion.fromJson(e)).toList());
+  
+}
