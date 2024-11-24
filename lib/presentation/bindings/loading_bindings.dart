63d3745af300e79d738a79e4aae1670795d643a1
Mateusz Dokowicz <matdok@st.amu.edu.pl>
2024-11-24 18:02:13 +0100

    remove unused _initialSurveyService

--- lib/presentation/bindings/loading_bindings.dart
+++ lib/presentation/bindings/loading_bindings.dart
@@ -1,14 +1,13 @@
 import 'package:get/get.dart';
 import 'package:survey_frontend/presentation/controllers/loading_controller.dart';
 
-class LoadingBindings extends Bindings{
+class LoadingBindings extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => LoadingController(
-      Get.find(), 
-      Get.find(),
-      Get.find(),
-      Get.find()
-    ));
+          Get.find(),
+          Get.find(),
+          Get.find(),
+        ));
   }
 }
