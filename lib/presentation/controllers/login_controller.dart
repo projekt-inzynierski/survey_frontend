9d3a68a02a70c5c50242c515631e7a22f5fcc738
pawelFelcyn <89747833+pawelFelcyn@users.noreply.github.com>
2024-12-14 17:39:22 +0100

    fix privacy policy encoding (#29)

--- lib/presentation/controllers/login_controller.dart
+++ lib/presentation/controllers/login_controller.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
@@ -17,14 +18,19 @@ class LoginController extends ControllerBase {
   final NeedInsertRespondentDataUseCase _needInsertRespondentDataUseCase;
   final GetStorage _storage;
   String? apiUrl;
-  final RegExp apiUrlRegex = RegExp(
-      r'https?:\/\/(www\\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)(:\d+)?');
+  late RegExp apiUrlRegex = RegExp(
+      r'(https:\/\/)?(www\\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)(:\d+)?');
   bool isBusy = false;
   bool _alwaysValidateInvalidCredentials = false;
   final Dio _dio;
 
   LoginController(this._loginService, this._storage,
-      this._needInsertRespondentDataUseCase, this._dio);
+      this._needInsertRespondentDataUseCase, this._dio){
+        if (kDebugMode){
+          apiUrlRegex = RegExp(
+          r'(https?)?:\/\/(www\\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)(:\d+)?');
+        }
+      }
 
   void login() async {
     if (isBusy) {
@@ -141,6 +147,9 @@ class LoginController extends ControllerBase {
   }
 
   void _saveUrl() {
+    if (!apiUrl!.startsWith('http')){
+      apiUrl = 'https://${apiUrl!}';
+    }
     _storage.write('apiUrl', apiUrl);
     _dio.options.baseUrl = apiUrl!;
   }
