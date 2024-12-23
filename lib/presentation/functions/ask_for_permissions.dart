bba7a14362c48d4335ddf4ff1c05d23713767a78
Mathew <106106919+mdokusV@users.noreply.github.com>
2024-12-23 10:50:17 +0100

    fix permission request (#43)

--- lib/presentation/functions/ask_for_permissions.dart
+++ lib/presentation/functions/ask_for_permissions.dart
@@ -1,6 +1,4 @@
-import 'package:geolocator/geolocator.dart';
 import 'package:permission_handler/permission_handler.dart';
-import 'package:survey_frontend/domain/local_services/notification_service.dart';
 import 'package:survey_frontend/presentation/screens/home/widgets/request.dart';
 
 Future<void> askForPermissions() async {
@@ -8,16 +6,13 @@ Future<void> askForPermissions() async {
     Permission.bluetooth,
     Permission.bluetoothScan,
     Permission.bluetoothConnect,
-    // Permission.locationAlways,
-    // Permission.locationWhenInUse
+    Permission.notification,
+    Permission.location,
   ].request();
-  // if (statuses.values.any((status) => !status.isGranted)) {
-  //   await buildManyDenyDialog();
-  // }
-  // if (!await Permission.locationAlways.isGranted) {
-  //   await buildLocationDenyDialog();
-  // }
-
-  await Geolocator.requestPermission();
-  await NotificationService.initialize();
+  if (statuses.values.any((status) => !status.isGranted)) {
+    await buildManyDenyDialog();
+  }
+  if (!await Permission.locationAlways.isGranted) {
+    await buildLocationDenyDialog();
+  }
 }
