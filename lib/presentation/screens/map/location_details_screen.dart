aeaa0545760ca43c0f4b4749791f51299aea9243
Paweł Felcyn <pawel.felcyn@onet.pl>
2025-01-02 15:53:32 +0100

    fix location details view

--- lib/presentation/screens/map/location_details_screen.dart
+++ lib/presentation/screens/map/location_details_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:survey_frontend/data/models/location_model.dart';
 import 'package:survey_frontend/l10n/get_localizations.dart';
-import 'package:survey_frontend/presentation/app_styles.dart';
 import 'package:survey_frontend/presentation/functions/formatters.dart';
 
 class LocationDetailsScreen extends StatelessWidget {
@@ -15,78 +14,76 @@ class LocationDetailsScreen extends StatelessWidget {
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Expanded(
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                TextFormField(
-                  readOnly: true,
-                  decoration: InputDecoration(
-                      label: Text(getAppLocalizations().latidude)),
-                  initialValue: model.latitude.toString(),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  readOnly: true,
-                  decoration: InputDecoration(
-                      label: Text(getAppLocalizations().longitude)),
-                  initialValue: model.longitude.toString(),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  readOnly: true,
-                  decoration:
-                      InputDecoration(label: Text(getAppLocalizations().date)),
-                  initialValue: dateOnlyShortFormat(model.dateTime),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                TextFormField(
-                  readOnly: true,
-                  decoration:
-                      InputDecoration(label: Text(getAppLocalizations().time)),
-                  initialValue: timeOnlyShortFormat(model.dateTime),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      model.sentToServer ? Icons.info_rounded : Icons.warning,
-                      color: model.sentToServer ? Colors.blue : Colors.red,
-                      size: 40,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextFormField(
+                readOnly: true,
+                decoration: InputDecoration(
+                    label: Text(getAppLocalizations().latidude)),
+                initialValue: model.latitude.toString(),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                readOnly: true,
+                decoration: InputDecoration(
+                    label: Text(getAppLocalizations().longitude)),
+                initialValue: model.longitude.toString(),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                readOnly: true,
+                decoration:
+                    InputDecoration(label: Text(getAppLocalizations().date)),
+                initialValue: dateOnlyShortFormat(model.dateTime),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                readOnly: true,
+                decoration:
+                    InputDecoration(label: Text(getAppLocalizations().time)),
+                initialValue: timeOnlyShortFormat(model.dateTime),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    model.sentToServer ? Icons.info_rounded : Icons.warning,
+                    color: model.sentToServer ? Colors.blue : Colors.red,
+                    size: 40,
+                  ),
+                  const SizedBox(
+                    width: 20,
+                  ),
+                  Flexible(
+                    child: Text(
+                      model.sentToServer
+                          ? getAppLocalizations()
+                              .theLocationDataHasBeenSubmitedToServer
+                          : getAppLocalizations()
+                              .theLocationDataHasNotBeenSubmitedToServer,
+                      style: const TextStyle(fontSize: 25),
                     ),
-                    const SizedBox(
-                      width: 20,
-                    ),
-                    Flexible(
-                      child: Text(
-                        model.sentToServer
-                            ? getAppLocalizations()
-                                .theLocationDataHasBeenSubmitedToServer
-                            : getAppLocalizations()
-                                .theLocationDataHasNotBeenSubmitedToServer,
-                        style: const TextStyle(fontSize: 25),
-                      ),
-                    )
-                  ],
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Divider(color: Colors.black),
-                const SizedBox(
-                  height: 10,
-                ),
-                _buildRelatedWithSurveyWidget()
-              ],
-            ),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Divider(color: Colors.black),
+              const SizedBox(
+                height: 10,
+              ),
+              _buildRelatedWithSurveyWidget()
+            ],
           ),
         ),
       ),
@@ -112,7 +109,7 @@ class LocationDetailsScreen extends StatelessWidget {
         ),
         Flexible(
           child: Text(
-             getAppLocalizations().surveyHasBeenComplitedInThisLocation,
+            getAppLocalizations().surveyHasBeenComplitedInThisLocation,
             style: const TextStyle(fontSize: 25),
           ),
         )
