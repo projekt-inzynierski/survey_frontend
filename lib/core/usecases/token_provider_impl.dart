b742bb7712ef91a10e41328f24eb7a39f5ce6e09
Paweł Felcyn <pawel.felcyn@onet.pl>
2025-01-03 18:04:22 +0100

    properly display profile when no init survey

--- lib/core/usecases/token_provider_impl.dart
+++ lib/core/usecases/token_provider_impl.dart
@@ -1,4 +1,5 @@
 import 'package:get_storage/get_storage.dart';
+import 'package:jwt_decoder/jwt_decoder.dart';
 import 'package:survey_frontend/domain/usecases/token_provider.dart';
 
 class TokenProviderImpl implements TokenProvider{
@@ -9,6 +10,18 @@ class TokenProviderImpl implements TokenProvider{
   @override
   String? getToken() {
     return _storage.read<String>('apiToken');
+  }
+  
+  @override
+  String? getUsername() {
+    final token = getToken();
+
+    if (token == null) {
+      return null;
+    }
+
+    final decoded = JwtDecoder.decode(token);
+    return decoded['sub'];
   }
 
 }
