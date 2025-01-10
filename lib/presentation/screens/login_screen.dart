10bc8e9c8bbe9022db6ad079f6a02d30bd0fce28
Mateusz Dokowicz <matdok@st.amu.edu.pl>
2025-01-10 17:54:16 +0100

    better API input keyboard

--- lib/presentation/screens/login_screen.dart
+++ lib/presentation/screens/login_screen.dart
@@ -29,7 +29,7 @@ class LoginScreen extends GetView<LoginController> {
                         height: 10,
                       ),
                       TextFormField(
-                        //TODO dont show word suggestions in keyboard
+                        keyboardType: TextInputType.visiblePassword,
                         decoration: InputDecoration(
                           labelText: AppLocalizations.of(context)!.apiUrl,
                         ),
